a9b1998a0e6cca89e932c1051b15a63ca6fa542a
Ahmed Rangel <ahmedrangel@outlook.com>
2026-03-08 16:49:03 -0500

    feat: add index landing spin wheel themed

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,15 +8,23 @@ watch(loggedIn, (value) => {
 </script>
 
 <template>
-  <UButton
-    label="Iniciar sesión con Twitch"
-    icon="simple-icons:twitch"
-    variant="soft"
-    color="neutral"
-    :ui="{
-      leadingIcon: 'text-white',
-      base: 'text-white text-md dark:bg-violet-600 light:bg-violet-800 dark:hover:bg-violet-700 light:hover:bg-violet-900',
-    }"
-    @click="openInPopup('/auth/twitch')"
-  />
+  <div class="w-screen h-screen flex flex-col items-center justify-center relative overflow-hidden">
+    <div class="w-screen h-screen spin-wheel-gradient" />
+    <div class="relative flex flex-col items-center gap-2 text-center text-white bg-neutral-900 p-4 rounded-lg spin-wheel-indicator border">
+      <p class="font-bold md:text-2xl">REWARDS WHEEL</p>
+      <div class="relative">
+        <UButton
+          label="Entrar con Twitch"
+          icon="simple-icons:twitch"
+          variant="soft"
+          color="neutral"
+          :ui="{
+            leadingIcon: 'md:w-8 md:h-8',
+            base: 'dark:bg-violet-600 light:bg-violet-800 dark:hover:bg-violet-700 light:hover:bg-violet-900 font-bold md:text-2xl py-3 px-2 md:py-5 md:px-6',
+          }"
+          @click="openInPopup('/auth/twitch')"
+        />
+      </div>
+    </div>
+  </div>
 </template>
