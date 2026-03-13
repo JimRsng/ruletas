ec0a05cd624b356fedc4abf6502f80b0052526e3
Yizack Rangel <yizackr@gmail.com>
2026-03-13 15:44:49 -0500

    chore: remove db files for now

--- .config/drizzle.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "sqlite",
-  schema: "./server/db/schema.ts",
-  out: "./server/db/migrations",
-  casing: "snake_case"
-});
